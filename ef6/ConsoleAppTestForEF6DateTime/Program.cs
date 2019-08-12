5b494079fd897b02a17b6ea4039801f62bf3fd8e
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-08-12 13:01:17 +0200

    DbFunctions.CreateDateTime(x.CheckinDate.Year, 1, 1, 0, 0, 0)

--- ef6/ConsoleAppTestForEF6DateTime/Program.cs
+++ ef6/ConsoleAppTestForEF6DateTime/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Linq;
 using System.Linq.Dynamic.Core;
 using ConsoleAppTestForEF6DateTime.EF6;
 
@@ -9,16 +11,18 @@ namespace ConsoleAppTestForEF6DateTime
         static void Main(string[] args)
         {
             Console.WriteLine("start");
-            var c = new MyHotelDbContext();
+            var ctx = new MyHotelDbContext();
 
-            var config = new ParsingConfig();
-            // config.EvaluateGroupByAtDatabase = false;
-
-            var reservations = c.Reservations.Select(config, "DateTime(CheckinDate.Year,1,1)");
-
-            foreach (var reservation in reservations)
+            var dates = ctx.Reservations.Select(x => DbFunctions.CreateDateTime(x.CheckinDate.Year, 1, 1, 0, 0, 0));
+            foreach (var date in dates)
             {
-                Console.WriteLine(reservation);
+                Console.WriteLine("Normal:" + date);
+            }
+
+            var datesDynamic = ctx.Reservations.Select("DbFunctions.CreateDateTime(CheckinDate.Year, 1, 1, 0, 0, 0)");
+            foreach (var date in datesDynamic)
+            {
+                Console.WriteLine("Dynamic:" + date);
             }
         }
     }
